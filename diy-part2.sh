25e80bd49614b515eb9bf109f0bc942094f82c38
未来 <aiastia@users.noreply.github.com>
2021-08-12 19:32:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,4 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git  package/lean/luci-app-smartdns
 #git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+chmod -R 755 files
