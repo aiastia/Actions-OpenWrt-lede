ca09e49f0312f311379d2673d159b3aec7193317
未来 <aiastia@users.noreply.github.com>
2022-02-06 10:34:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git diskman https://github.com/lisaac/luci-app-diskman' >>feeds.conf.default
+echo 'src-git luci https://github.com/openwrt/luci.git' >>feeds.conf.default
