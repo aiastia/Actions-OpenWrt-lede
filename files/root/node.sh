c61104d9283f6619a2df275da31e800e7dba6b67
未来 <aiastia@users.noreply.github.com>
2022-03-22 11:16:09 +0800

    Update node.sh

--- files/root/node.sh
+++ files/root/node.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sleep 120s
 
-nohup /root/status-client -dsn wss://openwrt:123123@node.aiastia.com >/dev/null 2>&1 &
+nohup /root/status-client -dsn wss://openwrt:123123@node >/dev/null 2>&1 &
