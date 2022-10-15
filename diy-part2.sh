d6af1e7eb7c4cfa2b2c9d852e8f963d3f008bd15
未来 <aiastia@users.noreply.github.com>
2022-10-15 12:10:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,3 +23,6 @@ git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-adv
 git clone https://github.com/sbwml/luci-app-alist package/alist
 #chmod -R 755 files
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+#golang19
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
