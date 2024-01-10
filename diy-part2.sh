bab932ad681709682045503d4eca95f3b5374870
未来 <aiastia@users.noreply.github.com>
2024-01-10 10:09:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,15 +14,16 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #rm -rf package/lean/luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git  package/lean/luci-app-smartdns
 #git clone https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
-git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
+git clone  --depth=1 https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-git clone https://github.com/sbwml/luci-app-alist package/alist
+git clone  --depth=1  https://github.com/sbwml/luci-app-alist package/alist
 #chmod -R 755 files
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 #golang19
 rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+# svn export https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
