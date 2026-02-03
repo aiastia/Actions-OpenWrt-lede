cd0b66aecfb665466008ce67d8336004bad24e12
未来 <aiastia@users.noreply.github.com>
2026-02-03 18:06:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,9 +28,9 @@ echo 'src-git asswall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.
 echo 'src-git openwrt_passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 
 # 常用插件源
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 # 其他工具
 ## echo 'src-git eqosplus https://github.com/sirpdboy/luci-app-eqosplus' >>feeds.conf.default
