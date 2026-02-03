09b85c2131d3f8da2680cb545965b861e3098ce2
未来 <aiastia@users.noreply.github.com>
2026-02-03 17:16:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -36,4 +36,4 @@ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.con
 echo 'src-git alist_luci https://github.com/sbwml/openwrt-alist' >>feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore main' >>feeds.conf.default  # 修正分支语法
 
-cat feeds.conf.default
+rm -rf feeds/smpackage/webd
