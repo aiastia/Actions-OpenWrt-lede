9f30de06447b56c3eee5f92eeededb098cd43cfd
未来 <aiastia@users.noreply.github.com>
2026-02-03 18:04:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,8 +20,9 @@
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default        # PassWall 2
 #echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default  # PassWall 1
 
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default  # PassWall 1
-echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default  # PassWall 1
+echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >>feeds.conf.default  # PassWall 1
+echo 'src-git asswall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >>feeds.conf.default  # PassWall 1
+
 
 # PassWall 依赖包
 echo 'src-git openwrt_passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
