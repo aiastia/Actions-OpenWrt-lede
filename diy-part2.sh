57c5a4dc433e948cc49ef6bf3a89975e2a4b5708
aiastia <aiastia@users.noreply.github.com>
2026-05-20 09:04:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -48,12 +48,12 @@ echo "Fixing luci-app-netdata..."
 sed -i '/pidof netdata/,+2d' feeds/luci/applications/luci-app-netdata/luasrc/controller/netdata.lua
 
 
-#vlmcsd 
-sed -i 's/^CONFIG_PACKAGE_vlmcsd=.*/# CONFIG_PACKAGE_vlmcsd is not set/' .config || echo "# CONFIG_PACKAGE_vlmcsd is not set" >> .config
-sed -i 's/^CONFIG_PACKAGE_luci-app-vlmcsd=.*/# CONFIG_PACKAGE_luci-app-vlmcsd is not set/' .config || echo "# CONFIG_PACKAGE_luci-app-vlmcsd is not set" >> .config
-rm -rf feeds/packages/net/vlmcsd
-rm -rf feeds/luci/applications/luci-app-vlmcsd
-# 如果你想排查被拉入的依赖，可以打印一下
-grep vlmcsd .config || echo "vlmcsd 已禁用"
+# #vlmcsd 
+# sed -i 's/^CONFIG_PACKAGE_vlmcsd=.*/# CONFIG_PACKAGE_vlmcsd is not set/' .config || echo "# CONFIG_PACKAGE_vlmcsd is not set" >> .config
+# sed -i 's/^CONFIG_PACKAGE_luci-app-vlmcsd=.*/# CONFIG_PACKAGE_luci-app-vlmcsd is not set/' .config || echo "# CONFIG_PACKAGE_luci-app-vlmcsd is not set" >> .config
+# rm -rf feeds/packages/net/vlmcsd
+# rm -rf feeds/luci/applications/luci-app-vlmcsd
+# # 如果你想排查被拉入的依赖，可以打印一下
+# grep vlmcsd .config || echo "vlmcsd 已禁用"
 
 
