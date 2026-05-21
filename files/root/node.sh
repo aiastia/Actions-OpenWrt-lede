3571afed01e7d2294607b25767d2ba218199a4ac
aiastia <aiastia@users.noreply.github.com>
2026-05-21 11:53:31 +0800

    Update node.sh

--- files/root/node.sh
+++ files/root/node.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
-sleep 10s
-nohup /root/status-client -dsn "$STATUS_DSN" >/dev/null 2>&1 &
+# sleep 是为了保证系统网络服务先启动
+sleep 60s
+
+# 启动 cloudflared
+if [ -n "$CLOUDFLARED_TOKEN" ]; then
+    nohup /root/cloudflared tunnel --no-autoupdate run --token "$CLOUDFLARED_TOKEN" >/dev/null 2>&1 &
+else
+    echo "CLOUDFLARED_TOKEN 未设置，cloudflared 未启动"
+fi
+
+# 启动 status-client
+if [ -n "$STATUS_DSN" ]; then
+    nohup /root/status-client -dsn "$STATUS_DSN" >/dev/null 2>&1 &
+else
+    echo "STATUS_DSN 未设置，status-client 未启动"
+fi
