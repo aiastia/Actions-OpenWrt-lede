f7957fed9db501fd3e558c30c3c820dc7e1042d0
aiastia <aiastia@users.noreply.github.com>
2026-05-21 11:49:49 +0800

    Update cf.sh

--- files/root/cf.sh
+++ files/root/cf.sh
@@ -3,4 +3,4 @@
 # kill 12345
 
 sleep 120s
-nohup /root/cloudflared tunnel --no-autoupdate run --token token >/dev/null 2>&1 &
+nohup /root/cloudflared tunnel --no-autoupdate run --token "$CLOUDFLARED_TOKEN" >/dev/null 2>&1 &
